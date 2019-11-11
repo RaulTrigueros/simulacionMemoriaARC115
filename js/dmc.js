d854456d816e695ae4cab250442b3449fc183c67
AlexVilleda <44938552+AlexVilleda@users.noreply.github.com>
2019-11-11 00:38:56 -0600

    Traducido el dmc.js
    Se tradujo un alert que faltaba

--- js/dmc.js
+++ js/dmc.js
@@ -261,6 +261,7 @@ function storeInstruction(){
 	if (document.getElementById("instruction_data").disabled==false)
 	{
 		document.getElementById('instruction_data').focus();	
+		alert("Por favor envíe la intrucción de almacenamiento");
 	}
 	else{
 	//Check write policy
